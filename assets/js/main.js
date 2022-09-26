ff08bd5ea68b497cae6306109b62a6bd9474e283
matthew-woodington <smwoodington@gmail.com>
2022-09-26 14:57:06 -0400

    remove key access

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,65 @@
-import { userData } from "./user.js";
-import { orgsData } from "./orgs.js";
-import { repoData } from "./repo.js";
+import { GITHUB_TOKEN } from "./token.js";
 
-/// repo number ///
-const sourceNum = document.querySelector("#repo-num-template").innerHTML;
-const templateNum = Handlebars.compile(sourceNum);
-const htmlNum = templateNum(userData);
-document.querySelector(".repo-page").insertAdjacentHTML("beforeend", htmlNum);
+function generateUser(data) {
+  const source = document.querySelector("#user-sidebar-template").innerHTML;
+  const template = Handlebars.compile(source);
+  const html = template(data);
+  document.querySelector(".sidebar").insertAdjacentHTML("afterbegin", html);
+}
 
-/// user ///
-const sourceUser = document.querySelector("#user-sidebar-template").innerHTML;
-const templateUser = Handlebars.compile(sourceUser);
-const htmlUser = templateUser(userData);
-document.querySelector(".sidebar").insertAdjacentHTML("afterbegin", htmlUser);
+function generateRepoNum(data) {
+  const numSource = document.querySelector("#repo-num-template").innerHTML;
+  const numTemplate = Handlebars.compile(numSource);
+  const numHtml = numTemplate(data);
+  document.querySelector(".repo-page").insertAdjacentHTML("beforeend", numHtml);
+}
 
-/// orgs ///
-const sourceOrgs = document.querySelector("#organizations-template").innerHTML;
-const templateOrgs = Handlebars.compile(sourceOrgs);
-const htmlOrgs = templateOrgs(orgsData);
-document.querySelector(".organizations").insertAdjacentHTML("afterbegin", htmlOrgs);
+fetch("https://api.github.com/users/matthew-woodington")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
 
-/// repo ///
-const sourceRepo = document.querySelector("#repo-list-template").innerHTML;
-const templateRepo = Handlebars.compile(sourceRepo);
-const repos = {
-  repoData,
-};
-const htmlRepo = templateRepo(repos);
-document.querySelector(".repository-list").insertAdjacentHTML("afterbegin", htmlRepo);
+    generateUser(data);
+    generateRepoNum(data);
+  });
 
-const codeType = document.querySelector(".code-type");
-const codeColor = document.querySelector(".code-color");
+function generateOrgs(data) {
+  const source = document.querySelector("#organizations-template").innerHTML;
+  const template = Handlebars.compile(source);
+  const html = template({ orgs: data });
+  document.querySelector(".organizations").insertAdjacentHTML("afterbegin", html);
+}
 
-function changeLanguageColor() {}
+fetch("https://api.github.com/users/matthew-woodington/orgs")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
 
-changeLanguageColor();
+    generateOrgs(data);
+  });
+
+function generateRepos(data) {
+  const source = document.querySelector("#repo-list-template").innerHTML;
+  const template = Handlebars.compile(source);
+  const html = template({ repos: data });
+  document.querySelector(".repository-list").insertAdjacentHTML("afterbegin", html);
+}
+
+fetch("https://api.github.com/users/matthew-woodington/repos")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+
+    generateRepos(data);
+  });
+
+function changeDateFormat(data) {
+  let repoList = data.map((object) => (object.updated_at = object.updated_at.toLocaleString()));
+  return repoList;
+}
--- assets/js/orgs.js
+++ assets/js/orgs.js
@@ -1,14 +1,14 @@
-export const orgsData = {
-  login: "ccs-summer-2022",
-  id: 112399320,
-  node_id: "O_kgDOBrMT2A",
-  url: "https://api.github.com/orgs/ccs-summer-2022",
-  repos_url: "https://api.github.com/orgs/ccs-summer-2022/repos",
-  events_url: "https://api.github.com/orgs/ccs-summer-2022/events",
-  hooks_url: "https://api.github.com/orgs/ccs-summer-2022/hooks",
-  issues_url: "https://api.github.com/orgs/ccs-summer-2022/issues",
-  members_url: "https://api.github.com/orgs/ccs-summer-2022/members{/member}",
-  public_members_url: "https://api.github.com/orgs/ccs-summer-2022/public_members{/member}",
-  avatar_url: "https://avatars.githubusercontent.com/u/112399320?v=4",
-  description: null,
-};
+// export const orgsData = {
+//   login: "ccs-summer-2022",
+//   id: 112399320,
+//   node_id: "O_kgDOBrMT2A",
+//   url: "https://api.github.com/orgs/ccs-summer-2022",
+//   repos_url: "https://api.github.com/orgs/ccs-summer-2022/repos",
+//   events_url: "https://api.github.com/orgs/ccs-summer-2022/events",
+//   hooks_url: "https://api.github.com/orgs/ccs-summer-2022/hooks",
+//   issues_url: "https://api.github.com/orgs/ccs-summer-2022/issues",
+//   members_url: "https://api.github.com/orgs/ccs-summer-2022/members{/member}",
+//   public_members_url: "https://api.github.com/orgs/ccs-summer-2022/public_members{/member}",
+//   avatar_url: "https://avatars.githubusercontent.com/u/112399320?v=4",
+//   description: null,
+// };
